bdaced0121bee6de2e167a6d91fe0535b335c7b2
HermannSipowa <43829703+HermannSipowa@users.noreply.github.com>
2020-10-09 00:50:22 -0400

    update

---
+++ MatlabCode/HermannCodeMain.m
@@ -0,0 +1,433 @@
+%--------------------------------------------------------------------------
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%% Hermann Kaptui Sipowa
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%--------------------------------------------------------------------------
+clear all
+close all
+start_up
+format long e
+clc
+global mu_dot tspan
+
+%% Calculating the boundaries conditions
+%=======================================%
+
+mm = 6; % Size of the state space
+mu_Earth   = 3.986004415E5; % Earth gravitational parameter
+Num_Agent  = 6; % Number of agents in the system
+Num_Agent1 = 4; % Number of agents in the first final-orbit
+Num_Agent2 = 2; % Number of agents in the second final-orbit
+
+% Specifying the chief's orbit
+%~~~~~~~~~~~~~~~~~~~~~~~~~~~~~%
+a_chief      = 1.5E+4;   % Semi-major axis in Km
+e_chief      = 0.0;      % Eccentricity
+inc_chief    = 50;       % Inclination in deg0
+BigOmg_chief = 10;       % RAAN in deg
+LitOmg_chief = 10;       % AOP in deg
+M_chief      = 0;        % True anomaly in deg
+
+
+% Computing the nearly non-singular orbital elements
+%~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~%
+inc_chief = deg2rad(inc_chief); BigOmg_chief = deg2rad(BigOmg_chief);
+LitOmg_chief = deg2rad(LitOmg_chief); M_chief = deg2rad(M_chief);
+COE = [a_chief,e_chief,inc_chief,BigOmg_chief,LitOmg_chief,M_chief];
+[Position_target,Velocity_target]  = COEstoRV(COE,mu_Earth); f_chief = M_chief;
+q1_chief = e_chief*cos(LitOmg_chief); q2_chief = e_chief*sin(LitOmg_chief);
+theta_chief = f_chief+LitOmg_chief;
+
+% Specify the initial relative-orbital element of the deputies
+%~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~%
+dela = 0;
+dele = 1/(2*a_chief);
+deli = 1/(4*a_chief);
+delLitOmg = 0;
+delBigOmg = 0;
+delM = 0;
+
+delq1 = dele*cos(BigOmg_chief) - e_chief*sin(BigOmg_chief)*delLitOmg;
+delq2 = dele*sin(BigOmg_chief) + e_chief*cos(BigOmg_chief)*delLitOmg;
+deltheta = delLitOmg + delM; % Relative true latitude in rad
+delCOE = [dela, deltheta, deli, delq1, delq2, delBigOmg]';
+
+
+% Integrating the deputies' initial trajectory
+%~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~%
+Period = 2*pi*sqrt(a_chief^3/mu_Earth);
+IntTime = 1*Period;
+tspan  = linspace(0,IntTime,10000);
+options = odeset('RelTol',2.22045e-14,'AbsTol',2.22045e-30);
+mu_dot = sqrt(mu_Earth/a_chief^3); % Chief's mean motion
+OE_chief = [a_chief, theta_chief, inc_chief, q1_chief, q2_chief, BigOmg_chief];
+AMap = ForwardMapping(OE_chief, mu_Earth); % Linear mapping matrix
+Xo = AMap*delCOE;
+u = zeros(3,1);
+[time, X_nom] = ode113(@(t,X)CW_ode(t,X,u,mu_dot),tspan,Xo,options);
+
+Xo = nan(mm,Num_Agent);
+for i = 1:Num_Agent
+    idx = find(time <= i*IntTime/Num_Agent, 1,'last');
+    Xo(:,i) = X_nom(idx,:)';
+    % if i == 1
+    %     X0 = X_nom(idx,:)';
+    % elseif i == 4
+    %     Xf = X_nom(idx,:)';
+    % end
+end
+[~, X_rel] = ode113(@(t,x)CW_ode(t,x,u,mu_dot),tspan,Xo,options);
+
+
+% Specify the final relative-orbital elements of the deputies in both orbits
+%~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~%
+dela = 0;
+dele1 = 1/(6*a_chief);
+deli1 = 1/(2*a_chief);
+delLitOmg = 0;
+delBigOmg = 0;
+delM = 0;
+delq1_1 = dele1*cos(BigOmg_chief) - e_chief*sin(BigOmg_chief)*delLitOmg;
+delq2_1 = dele1*sin(BigOmg_chief) + e_chief*cos(BigOmg_chief)*delLitOmg;
+deltheta = delLitOmg + delM; % Relative true latitude in rad
+delCOE_1 = [dela, deltheta, deli1, delq1_1, delq2_1, delBigOmg]';
+
+delBigOmg = -pi*1E-5;
+delM = pi*1E-6;
+delLitOmg = pi*1E-5;
+deltheta = delLitOmg + delM; % Relative true latitude in rad
+dele2 = 1/(8*a_chief);
+deli2 = 2/(10*a_chief);
+delq1_2 = dele2*cos(BigOmg_chief) - e_chief*sin(BigOmg_chief)*delLitOmg;
+delq2_2 = dele2*sin(BigOmg_chief) + e_chief*cos(BigOmg_chief)*delLitOmg;
+delCOE_2 = [dela, deltheta, deli2, delq1_2, delq2_2, delBigOmg]';
+
+
+% Integrating the deputy final trajectories
+%~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~%
+Xo_1 = AMap*delCOE_1;
+u = zeros(3,1);
+[time, X_nom_1] = ode113(@(t,X)CW_ode(t,X,u,mu_dot),tspan,Xo_1,options);
+
+Xo_1 = nan(mm,Num_Agent1);
+for i = 1:Num_Agent1
+    idx = find(time <= i*IntTime/Num_Agent1, 1,'last');
+    Xo_1(:,i) = X_nom_1(idx,:)';
+end
+[~, X_rel_1] = ode113(@(t,x)CW_ode(t,x,u,mu_dot),tspan,Xo_1,options);
+
+Xo_2 = AMap*delCOE_2;
+[time, X_nom_2] = ode113(@(t,X)CW_ode(t,X,u,mu_dot),tspan,Xo_2,options);
+
+Xo_2 = nan(mm,Num_Agent2);
+for i = 1:Num_Agent2
+    idx = find(time <= (2*i-1)*IntTime/(2*Num_Agent2), 1,'last');
+    Xo_2(:,i) = X_nom_2(idx,:)';
+end
+[~, X_rel_2] = ode113(@(t,x)CW_ode(t,x,u,mu_dot),tspan,Xo_2,options);
+
+
+
+
+
+%% Geometric Motion Planning
+%===========================%
+%---------------------------- AGHF parameters ----------------------------
+% Boundary conditions
+jj = 2; idx = 1+mm*(jj-1):mm*jj; X0 = X_rel(1,idx)'; 
+jj = 1; idx = 1+mm*(jj-1):mm*jj; Xf = X_rel_1(1,idx)';
+% smax for steady state
+smax = 1e2;
+% # of grids in t
+tgrids = 200;
+% # of grids in s
+sgrids = 1000;
+% # of grids of integration for final trajectory extraction
+intgrids = 1e5;
+% # of states
+N = length(X0);
+% # of inputs
+M = 3;
+% motion duration
+T = IntTime;
+% penalty value (the lambda in paper)
+Penalty = 1e3; 
+
+% solve for trajectory, see "AGHF" function file for implementation details
+[p, dp, xfull, xdrift, bufull, budrift, sol, s, t, tint, cost,X_ode45,p_initial] = AGHF(smax, tgrids, intgrids, sgrids, Penalty, X0, Xf, T, N, M);
+
+
+%% plotting
+%===========================%
+%------------------- Plotting the extracted control -----------------------
+idx = 0:1:intgrids-1;
+figure('Name','Extracted control')
+plot(idx,budrift);
+set(gca, 'XScale', 'log')
+title('actuated curve length vs s')
+xlabel('s')
+ylabel('curve length')
+grid on;
+
+
+figure('Name','actuated curve length vs s')
+loglog(s,cost);
+title('actuated curve length vs s')
+xlabel('s')
+ylabel('curve length')
+grid on;
+
+% u1 = sol(:,:,1);    
+% u2 = sol(:,:,2);    
+% u3 = sol(:,:,3);    
+% u4 = sol(:,:,4);    
+% u5 = sol(:,:,5);   
+% 
+% figure('Name','3D configuration space curve deformation')
+% X=u1(1,:);
+% Y=u2(1,:);
+% Z=u3(1,:);
+% h1=plot3(X,Y,Z,'k','LineWidth',2);
+% hold on
+% plot3(X,Y,Z,'r','LineWidth',2);
+% legend('x-y-\theta trajectory','initial guess')
+% axis([min(u1(end,:))-0.1,max(u1(end,:))+0.1,min(u2(end,:))-0.1,max(u2(end,:))+0.1,min(u3(end,:))-0.1,max(u3(end,:))+0.1]);
+% grid ON;
+% title('3D configuration space curve deformation');
+% xlabel('x');
+% ylabel('y');
+% zlabel('\theta');
+% %pause;
+% 
+% for i=1:sgrids
+%     X=u1(i,:);h1.XDataSource='X';
+%     Y=u2(i,:);h1.YDataSource='Y';
+%     Z=u3(i,:);h1.ZDataSource='Z';
+%     refreshdata(h1,'caller');
+%     drawnow;
+%     pause(0.05);
+% end
+% 
+% figure('Name','xy-plane trajectory deformation')
+% 
+% X=u1(1,:);
+% Y=u2(1,:);
+% h1=plot(X,Y,'k','LineWidth',2);
+% hold on
+% plot(X,Y,'r','LineWidth',2);
+% legend('x-y trajectory','initial guess')
+% axis([min(u1(end,:))-0.1,max(u1(end,:))+0.1,min(u2(end,:))-0.1,max(u2(end,:))+0.1]);
+% grid ON;
+% title('xy-plane trajectory and unicycle deformation');
+% xlabel('x');
+% ylabel('y');
+% %pause;
+% 
+% for i=1:sgrids
+%     
+%     X=u1(i,:);h1.XDataSource='X';
+%     Y=u2(i,:);h1.YDataSource='Y';
+%     
+%     refreshdata(h1,'caller');
+%     drawnow;
+%     pause(0.05);
+%     
+% end
+
+%% animation
+for Penalty = 1
+c1 = rgb('Aqua');
+c2 = rgb('Gold');
+c3 = rgb('Lime');
+c4 = rgb('LightBlue');
+c5 = rgb('DarkBlue');
+c6 = rgb('Red');
+
+cMat1 = [c6;c5;c2;c3];
+cMat2 = [c1;c4];
+Label_1 = {'Agent1','Agent2','Agent3','Agent4'};
+Label_2 = {'Agent5','Agent6'};
+Label = {'Agent1','Agent2','Agent3','Agent4','Agent5','Agent6'};
+
+fig= figure();
+hold on
+for jj=1%:Num_Agent1
+    idx = 1+mm*(jj-1):mm*jj;
+
+    plot3(X_rel_1(:,idx(1)),X_rel_1(:,idx(2)),X_rel_1(:,idx(3)),'m-.');
+    plot3(X_rel_1(1,idx(1)),X_rel_1(1,idx(2)),X_rel_1(1,idx(3)),'o',...
+        'LineWidth',3,...
+        'MarkerEdgeColor',cMat1(jj,:),...
+        'MarkerFaceColor',cMat1(jj,:)',...
+        'MarkerSize',8);
+    hold on
+
+    xlabel('X [km]')
+    ylabel('Y [km]')
+    zlabel('Z [km]')
+    title('{\color{black} 3D Relative Trajectory}','interpreter','tex')
+    if jj == 1
+        Xf = X_rel_1(1,idx)';
+    end
+    % elseif jj == 2
+    %     Xf = [Xf;X_rel_1(1,idx1)'];
+    % end
+end
+
+
+%     for jj=1%:Num_Agent2
+%         idx2 = 1+mm*(jj-1):mm*jj;
+%         plot3(X_rel_2(:,idx2(1)),X_rel_2(:,idx2(2)),X_rel_2(:,idx2(3)),'m-.');
+%         plot3(X_rel_2(1,idx2(1)),X_rel_2(1,idx2(2)),X_rel_2(1,idx2(3)),'o',...
+%             'LineWidth',3,...
+%             'MarkerEdgeColor',cMat2(jj,:),...
+%             'MarkerFaceColor',cMat2(jj,:)',...
+%             'MarkerSize',8);
+%     end
+%     
+
+
+c1 = rgb('Aqua'); c2 = rgb('Gold'); c3 = rgb('Lime');
+c4 = rgb('LightBlue'); c5 = rgb('DarkBlue'); c6 = rgb('Red');
+c7 = rgb('Tomato');
+cMat = [c6;c5;c2;c3;c1;c4];
+plt   = zeros(Num_Agent);
+%     figure
+hold on
+for jj=2%:Num_Agent
+    idx = 1+mm*(jj-1):mm*jj;
+
+    % h4 = plot3(X_rel(:,idx(1)),X_rel(:,idx(2)),X_rel(:,idx(3)),'k-.','DisplayName','Chief');
+    plt(jj) = plot3(X_rel(1,idx(1)),X_rel(1,idx(2)),X_rel(1,idx(3)),'*',...
+        'LineWidth',3,...
+        'MarkerEdgeColor',cMat(jj,:),...
+        'MarkerFaceColor',cMat(jj,:)',...
+        'MarkerSize',8);
+
+    xlabel('X [km]')
+    ylabel('Y [km]')
+    zlabel('Z [km]')
+    title('{\color{black} 3D Relative Trajectory}','interpreter','tex')
+    % hL = legend(plt(1:jj),Label(1:jj));
+    % axis equal
+    % if jj == 5
+    %     X0 = X_rel(1,idx)';
+    % elseif jj == 6
+    %     X0 = [X0; X_rel(1,idx)'];
+    % end
+    if jj == 2
+        X0 = X_rel(1,idx)';
+    end
+end
+grid on
+h5 = plot3(0,0,0,'bo',...
+    'LineWidth',2,...
+    'MarkerEdgeColor','k',...
+    'MarkerFaceColor','k',...
+    'MarkerSize',15,'DisplayName','Chief');
+h4 = plot3(X_rel(:,idx(1)),X_rel(:,idx(2)),X_rel(:,idx(3)),'k-.','DisplayName','Initial Traj');
+h6 = plot3(X_rel_1(:,idx(1)),X_rel_1(:,idx(2)),X_rel_1(:,idx(3)),'m-.','DisplayName','Final Traj');
+
+% Programatically move the Legend
+newPosition = [.2 .7 0.05 0.05];
+newUnits = 'normalized';
+% set(hL,'Position', newPosition,'Units', newUnits,'NumColumns',1);
+view(31,18)
+    
+end
+
+% figure('Name','unicycle animation');
+plot3(p(1,:),p(2,:),p(3,:),'k','LineWidth',2);
+hold on;
+plot3(interp1(tint,xdrift(1,:),t),interp1(tint,xdrift(2,:),t),interp1(tint,xdrift(3,:),t),':c','LineWidth',2);
+plot3(p_initial(1,:),p_initial(2,:),p_initial(3,:),'g','LineWidth',2);
+plot3(interp1(tint,X_ode45(1,:),t),interp1(tint,X_ode45(2,:),t),interp1(tint,X_ode45(3,:),t),'--r','LineWidth',2);
+% legend('AGHF solution','initial Condition', 'integrated path',  'integrated ODE45', 'AutoUpdate','off')
+
+%%
+figure
+plot3(p(1,:),p(2,:),p(3,:),'k','LineWidth',2);
+hold on
+plot3(p_initial(1,:),p_initial(2,:),p_initial(3,:),'g','LineWidth',2);
+grid on
+
+%%
+figure
+plot3(p(4,:),p(5,:),p(6,:),'k','LineWidth',2);
+hold on
+plot3(p_initial(4,:),p_initial(5,:),p_initial(6,:),'g','LineWidth',2);
+grid on
+%%
+%plot actual trajectory
+% Xs=interp1(tint,xdrift(1,:),t);
+% Ys=interp1(tint,xdrift(2,:),t);
+% Zs=interp1(tint,xdrift(3,:),t);
+% Z1s=interp1(tint,xdrift(4,:),t);
+% Z2s=interp1(tint,xdrift(5,:),t);
+% 
+% w = 0.2;
+% h = 0.1;
+% Rot_s = [cos(Zs(1)) -sin(Zs(1));
+%        sin(Zs(1)) cos(Zs(1))];
+% p1s = [Xs(1),Ys(1)]' + Rot_s*[w/2;h/2];
+% p2s = [Xs(1),Ys(1)]' + Rot_s*[-w/2;h/2];
+% p3s = [Xs(1),Ys(1)]' + Rot_s*[-w/2;-h/2];
+% p4s = [Xs(1),Ys(1)]' + Rot_s*[w/2;-h/2];
+% h_box_patch=patch([p1s(1) p2s(1) p3s(1) p4s(1)], [p1s(2) p2s(2) p3s(2) p4s(2)],[0.4,0.4,1]);
+% h_v=quiver(Xs(1),Ys(1),Z1s(1)*cos(Zs(1)),Z1s(1)*sin(Zs(1)),'r');
+% qfactor = 0.05;
+% qhead = 10;
+% qwidth = 1.5;
+% set(h_v,'AutoScale','off','AutoScaleFactor',qfactor,'MaxHeadSize',qhead,'linewidth',qwidth)
+% 
+% axis equal
+% axis([-1.1,1.1,-1.6,0.6]);
+% grid ON;
+% xlabel('x');
+% ylabel('y');
+% %pause;
+% 
+% ind_plot = 0:10:tgrids;
+% ind_plot(1) = 1;
+% ind_count = 1;
+% for i=1:tgrids
+%     delete(h_box_patch);
+%     delete(h_v);
+%     Rot_s = [cos(Zs(i)) -sin(Zs(i));
+%         sin(Zs(i)) cos(Zs(i))];
+%     p1s = [Xs(i),Ys(i)]' + Rot_s*[w/2;h/2];
+%     p2s = [Xs(i),Ys(i)]' + Rot_s*[-w/2;h/2];
+%     p3s = [Xs(i),Ys(i)]' + Rot_s*[-w/2;-h/2];
+%     p4s = [Xs(i),Ys(i)]' + Rot_s*[w/2;-h/2];
+%     pxs = [p1s(1) p2s(1) p3s(1) p4s(1) p1s(1)];
+%     pys = [p1s(2) p2s(2) p3s(2) p4s(2) p1s(2)];
+%     
+%     h_box_patch=patch(pxs(1:4),pys(1:4),[0.4,0.4,1]);
+%     h_v=quiver(Xs(i),Ys(i),Z1s(i)*cos(Zs(i)),Z1s(i)*sin(Zs(i)),'r');
+%     set(h_v,'AutoScaleFactor',qfactor,'MaxHeadSize',qhead,'linewidth',qwidth)
+%     
+%     if i == ind_plot(ind_count)
+%         patch(pxs(1:4),pys(1:4),[0.4,0.4,1],'EdgeAlpha', 0.4+(i/tgrids)*0.6, 'FaceAlpha', 0.4+(i/tgrids)*0.6);
+%         U = Z1s(i)*cos(Zs(i));
+%         V = Z1s(i)*sin(Zs(i));
+%         if (abs(U)>0.0001)&&(abs(V)>0.0001)
+%             quiver(Xs(i),Ys(i),U,V,'r','AutoScaleFactor',qfactor,'MaxHeadSize',qhead,'linewidth',qwidth);
+%         end
+%         ind_count = ind_count + 1;
+%     end
+% 
+%     drawnow;
+%     pause(0.005)
+% end
+% 
+% delete(h_box_patch);
+% delete(h_v);
+
+
+
+
+
+
+
+
+
---
+++ MatlabCode/unicycle_drift_AGHF.m
@@ -0,0 +1,278 @@
+function [p, dp, xfull, xdrift, bufull, budrift, sol, t, x, xnew, cost, X_ode45] = unicycle_drift_AGHF(tmax, xpoints, intgrids, tpoints, k, X, Xf, T, N, M)
+% unicycle_drift_AGHF Solve the AGHF for dynamical unicycle
+% code auther: Yinai Fan
+%
+% system: dot_x = f(x) + F(x)*u
+% Lagragian of actuated curve length: (x_dot-f(x))'*G(x_dot-f(x))
+%
+% The AGHF is equivalent as the Euler Lagrange equation:
+% dx/ds = AGHF  <===> dx/ds = - (dL/dx-d(dL/d(dot_x))/dt)
+% In this implementation, the EL equation is used for solving PDE
+%
+% Inputs:
+% tmax -- smax
+% xpoints -- number of grids in time t
+% intgrids -- number of grids for integration of solution
+% tpoints -- number of grids in s
+% k -- penalty (lambda is used in paper)
+% X0, Xf -- initial and final value of states
+% T -- motion duration
+%
+% Outputs:
+% p, dp -- steady state PDE solution for x, dot_x of the AGHF
+% xfull -- state trajectory integrated by using full control (virtual control included)
+% xdrift -- solution of the motion, state trajectory integrated by using actual control
+% bufull -- full control (virtual control included)
+% budrift -- actual control
+% sol -- raw solution of pdepe
+% t -- s grids vector for solving AGHF,  x -- time grids vector for solving AGHF
+% xnew -- time vector for integration of solutions
+% cost -- actuated curve length values along s grids
+
+%----------------------- pdepe setup ------------------------------
+m = 0;
+x = linspace(0,T,xpoints);                  % discretization of the curve in time
+t = [0 logspace(-4,log10(tmax),tpoints-1)]; % discretization of s interval in log scale
+
+% global variable for the Euler Lagrange function and Metric G
+global get_EL get_G
+
+% generate functions for EL, metric G, drift term f, control matrix F
+% please modify this function if system is changed
+[get_EL, get_G, get_f, get_F] = generate_fh_of_model(N,M); 
+
+opts = odeset('AbsTol',1e-14);
+% opts = odeset('RelTol',2.22045e-14,'AbsTol',2.22045e-30);
+tic;
+disp('solving PDE...');
+% Solve AGHF
+sol = pdepe(m,@(x,t,u,DuDx) mypdexpde(x,t,u,DuDx,k,N),...
+              @(x) mypdexic(x, X, Xf, T),...
+              @(xl,ul,xr,ur,t) mypdexbc(xl,ul,xr,ur,t, X, Xf, N),...
+              x,t,opts);
+% The solution "sol" is of form sol(t,x,i)
+toc;
+
+%------------------ Extract Controls and state trajectory------------------
+tic;
+disp('Extracting Trajectory...');
+
+% # of integration grids
+xpoints_new = intgrids;
+% initialize controls and states
+budrift = zeros(M,xpoints_new); % actual control
+bufull = zeros(N,xpoints_new); % full control (control on unadmissible directions included!!!)
+xnew = linspace(0,T,xpoints_new); % time grids for integration
+
+% use spline to interpolate state from steady state pde solution, p
+p = zeros(N,xpoints_new);
+for i = 1:N
+    p(i,:)=spline(x,sol(end,:,i),xnew);
+end
+% initial value of drift term f(x(0))
+drift0 = get_f(p(:,1));
+% manualy defferentiate to get solution of x_dot, represented by dp
+dp = [drift0 diff(p.').'/(T/(xpoints_new-1))];
+
+% control extraction
+for i = 1 : xpoints_new
+    % f(x(t)), note: the F_d(x) in paper
+    drift = get_f(p(:,1));
+    % get [Fc F], note: the F_bar matrix in paper
+    Ffull = get_F(p(:,1));
+    % get full control
+    bufull(:,i) = Ffull^(-1) * ( dp(:,i) - drift );
+    % get actual control
+    budrift(:,i)= bufull(N-M+1:end,i);
+end
+
+
+%Integrate to get state trajectory
+
+% initialize state trajctroy with full control
+xfull = zeros(N,xpoints_new);
+%Initial state
+xfull(:,1) = p(:,1); 
+% initialize state trajctroy with actual control
+xdrift = zeros(N,xpoints_new);
+X_ode45 = zeros(N,xpoints_new);
+%Initial state
+xdrift(:,1)= p(:,1);
+X_ode45(:,1) = p(:,1);
+tolerance = 1e-16;
+h = 1e-6;
+% Euler integration to get trajectory (can be replaced by ode45 to get better result)
+for i = 1:xpoints_new-1
+    %[Fc F]
+    cBfull = get_F(xfull(:,i));
+    % just F
+    cBdrift = cBfull(:,(end-M+1):end);
+    drift = get_f(xdrift(:,i));
+    drift_full = get_f(xfull(:,i));
+    xdrift(:,i+1) = xdrift(:,i) + ( drift + cBdrift*budrift(:,i) )*(xnew(i+1)-xnew(i));
+    xfull(:,i+1) = xfull(:,i) + ( drift_full + cBfull*bufull(:,i) )*(xnew(i+1)-xnew(i));
+    
+    
+    u = budrift(:,i);
+    [y_f, ~, h_next] = Runge_Kutta_Fehlberg_4_5(@(t,X)MyOde(t,X,u),X_ode45(:,i),xnew(i),h,xnew(i+1),tolerance);
+    h = h_next;
+    X_ode45(:,i+1) = y_f;
+end
+
+% initialize cost -- actuated curve length
+cost = zeros(1,tpoints);
+% calculate the atuated curve length for each grid of s
+X_temp = zeros(N,xpoints);
+dX_temp = zeros(N,xpoints);
+for j = 1:tpoints
+    for kk = 1:N
+    [X_temp(kk,:), dX_temp(kk,:)]=pdeval(m,x,sol(j,:,kk),x);
+    end
+    %for each s grid, integrate  (x_dot-f(x))'*G(x_dot-f(x)) to get curve
+    %length
+    for i = 1:xpoints
+        X = X_temp(:,i);
+        dX = dX_temp(:,i);
+        f = get_f(X);
+        G = get_G(X,k);
+        cost(j) = cost(j) + (dX-f)'*G*(dX-f)*(T/(xpoints-1));
+    end
+end
+toc;
+disp('Done!!!!!');
+
+end
+% --------------------------------------------------------------------------
+
+% The followings are the PDE, initial condition and boundary condition for pdepe:
+function [c,f,s] = mypdexpde(x,t,u,DuDx,k,N) % Define PDE; right-hand-side of AGHF
+
+global get_EL
+
+% evaluate EL
+EL = get_EL(u,DuDx,k);
+% dL/dx
+pLx = EL(:,1);
+% dL/d(dot_x)
+pLxd = EL(:,2);
+
+f = pLxd;
+s = -pLx;
+
+c = ones(N,1);
+
+% for details of f, s, c, please see the documentation for pdepe
+
+end
+% --------------------------------------------------------------------------
+ 
+function u0 = mypdexic(x, X0, Xf, T)    % Initial condition for AGHF
+
+% straight line connecting X0 and Xf
+u0=X0+(Xf-X0)*(x/T);
+% add some sinusoidal deviation to x1 initial guess (can be discarded)
+u0(1)=X0(1)+(Xf(1)-X0(1))*(x/T) + 0.01*sin(2*pi*x/T);
+
+end
+% --------------------------------------------------------------------------
+
+function [pl,ql,pr,qr] = mypdexbc(xl,ul,xr,ur,t, X0, Xf, N)    % Boundary condition for AGHF
+
+pl = ul-X0;
+ql = zeros(N,1);
+pr = ur-Xf;
+qr = zeros(N,1);
+
+end
+
+% generate functions for calculating EL equation:
+%!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+%!!!!!!!!!!! Need to modify this function if system is changed !!!!!!!!!!
+%!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+% Inputs:
+% N,M -- # of states and inputs
+% Output:
+% get_EL -- function handle for calculationg EL equation, it returns dL/dx
+% and % dL/d(dot_x), it takes x and x_dot as input
+% get_G -- function handle for calculating the metric G(x)
+% get_f -- function handle for calculating the drift term f(x)
+% get_F -- function handle for calculating the full control matrix [Fc(x) F(x)]
+function [get_EL, get_G, get_f, get_F] = generate_fh_of_model(N,M) 
+tic;
+disp('generating functions for pdepe...');
+
+% ------------------------- system setup ---------------------------------
+
+% make the state and state deirvative simbolic
+syms x y th z1 z2 k dx dy dth dz1 dz2 real
+X  = [x y th z1 z2]';
+Xd = [dx dy dth dz1 dz2]';
+
+% drift term f(x) note: Fd(x) is used in the paper
+f = [cos(th)*z1 sin(th)*z1 z2 0 0]';
+% [Fc F], note: the F_bar matrix in paper
+F = sym(eye(N));
+% penalty matrix (D matrix in the paper)
+D = diag([k*ones(1,N-M) ones(1,M)]);
+% the metric, before adding the state constraint barrier functions
+H = (F.')^(-1)*D*F^(-1);
+
+
+% -------------------- state constraint barrier function ------------------
+% B is a vector of barrier function for 1 state constraint, each scaler state
+% constraint need 1 barrier function
+B = [];
+% barrier function parameters
+kb = .01; % barrier function gain
+pb = 1; % order of barrier function
+
+% b is one penalty term for state constraint
+% b = 0; % no state contraints
+% b = kb/((2*pi/4)^2 - z2^2)^pb; %contraint on angular velocity, |z2|<pi/2
+% b = kb/(2^2 - z1^2)^pb; %contraint on translational velocity, |z1|<2
+% b = kb/(z1+2)^pb + kb/(2-z1)^pb; %contraint on translational velocity |z1|<2
+% B = [B b]; % attach the barrier function to B
+
+
+% -------------------- Metric and curve Length ----------------------------
+
+% the metric with state constraint barier functions
+G =(sum(B)+1)*H;
+% actuated curve length
+L =  simplify( (Xd - f).' * G * (Xd - f) );
+
+% -------------------- Function Generations -------------------------------
+
+% taking derivatives symbolically to get the EL euqation terms
+% pLx -- dL/dx
+% pLxd -- dL/d(x_dot)
+pLx = sym('pLx', [N 1],'real');
+pLxd = sym('pLxd', [N 1],'real');
+for i=1:N
+    pLx(i) =  diff(L,X(i));
+    pLxd(i) = diff(L,Xd(i));
+end
+
+% generate functions
+get_EL = matlabFunction([pLx, pLxd],'vars', {X,Xd,k});
+get_G = matlabFunction(G,'vars', {X,k});
+get_f = matlabFunction(f,'vars',{X});
+get_F = matlabFunction(F,'vars',{X});
+
+toc;
+end
+
+
+function [Xdot] = MyOde(t,X,u)
+th = X(3);
+z1 = X(4);
+z2 = X(5);
+f = [cos(th)*z1 sin(th)*z1 z2 0 0]';
+B = [0 0; 0 0; 0 0; 1 0; 0 1];
+Xdot = f + B*u;
+
+end
+
+
+
+
